fabace6fdb20c738c3c862585fae3ec831ecbc69
Parinika <parinikaprasad@gmail.com>
2026-09-20 13:31:51 +0530

    Add basic arithmetic program

---
+++ basicop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the operands: ";
+    cin>>a>>b;
+    cout<<"sum = "<<a+b<<endl;
+    cout<<"difference = "<<a-b<<endl;
+    cout<<"product = "<<a*b<<endl;
+    cout<<"quotient= "<<(float)a/b<<endl;
+    return 0;
+
+}
